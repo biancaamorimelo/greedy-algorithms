beb8b77e6efc8223b1bd7da66e9f5d388d98f26d
Bianca Amorim <biancamoriim@users.noreply.github.com>
2019-04-04 15:00:27 -0300

    Updating a few things
    Added a few comments

--- src/main/DijkstraAlgorithm.java
+++ src/main/DijkstraAlgorithm.java
@@ -12,16 +12,15 @@ import java.util.List;
 public class DijkstraAlgorithm {
 	
 	List<Vertice> vertices;
-
+	
+	// Construtor com parametros sendo o grafo representado em forma de lista de adjacência e em matriz
     public DijkstraAlgorithm(List<Vertice> adjacencia, int[][] representacaoMatriz) {
         this.vertices = adjacencia;
-
         this.iniciaVertices();
-
         this.melhoresCaminhos(this.vertices.toArray(new Vertice[0]), representacaoMatriz, 0, (this.vertices.size() - 1));
-
     }
 
+    // Inicializa todos os vértices com valor infinito
     public void iniciaVertices() {
         for (Vertice v : this.vertices) {
             v.valor = Integer.MAX_VALUE;
@@ -29,8 +28,9 @@ public class DijkstraAlgorithm {
     }
     
     private void melhoresCaminhos(Vertice[] vertices, int[][] representacaoMatriz, int inicio, int fim) {
-        vertices[inicio].valor = 0;
-
+        //Atribui zero ao primeiro vértice
+    	vertices[inicio].valor = 0;
+    	
         //Constroi a primeira arvore heap
         ArrayList<Vertice> minHeap = buildMinHeap(vertices.clone());
 
@@ -39,15 +39,13 @@ public class DijkstraAlgorithm {
         Vertice[] res = new Vertice[vertices.length];
 
         while (minHeap.size() != 0) { 
-            verticeAuxiliar = minHeap.remove(0); 
+            verticeAuxiliar = minHeap.remove(0); // Armazenar o menor elemento
 
             // Percorre os demais 
             for (Vertice v : verticeAuxiliar.adj) {
-                /* Relaxamento */
-
+                // Relaxamento 
                 if (minHeap.contains(v)
                         && v.valor > verticeAuxiliar.valor + representacaoMatriz[verticeAuxiliar.getId()][v.getId()]) {
-
                     v.valor = (verticeAuxiliar.valor + representacaoMatriz[verticeAuxiliar.getId()][v.getId()]);
                     v.pai = verticeAuxiliar;
                 }
@@ -57,7 +55,7 @@ public class DijkstraAlgorithm {
             res[representacaoMatriz.length - (minHeap.size() + 1)] = verticeAuxiliar;
         }
 
-        System.out.println("Menor caminho do " + inicio + " para " + fim + " e: ");
+        System.out.println("Menor caminho do " + inicio + " para " + fim + ": ");
         String resposta = "";
         int caminhoTotal = 0;
 
@@ -69,11 +67,11 @@ public class DijkstraAlgorithm {
         }
         resposta = verticeAuxiliar.getId() + "";
         while (verticeAuxiliar.pai != null) {
-            resposta = verticeAuxiliar.pai.getId() + " - " + resposta;
-            caminhoTotal+=representacaoMatriz[verticeAuxiliar.pai.id][verticeAuxiliar.id];
+            resposta = verticeAuxiliar.pai.getId() + " - " + resposta;  
+            caminhoTotal+=representacaoMatriz[verticeAuxiliar.pai.id][verticeAuxiliar.id];  // Realiza a soma dos caminhos percorridos
             verticeAuxiliar = verticeAuxiliar.pai;
         }
-        System.out.println(resposta+
-                "\n Soma dos caminhos = " + caminhoTotal);        
+        
+        System.out.println(resposta + "\n Soma dos caminhos = " + caminhoTotal);        
     }
 }
